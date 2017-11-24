67ac05cc0164abf7816e3cf909357fd9ee94ed02
CodeViking13 <92metaller@gmail.com>
2017-11-25 01:47:58 +0400

    trying to fit into observable.

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,19 @@
-import rx from "rxjs";
+import Rx from "rxjs";
 
-console.log(rx);
+const btn = document.querySelector("#btn");
+
+const observeble = Rx.Observable.create(observer => {
+    observer.next(1);
+    setTimeout(() => {
+        observer.next(4);
+    }, 1000);
+});
+
+btn.onclick = () => {
+    let count = 0;
+    observeble.next(++count);
+};
+
+observeble.subscribe(x => {
+    console.log("got value", x);
+});
