fcc7993778d2efa1ae1271bb86099a600f9797cb
dpischalka <pischalkads@altarix.ru>
2017-11-27 13:45:05 +0400

    first practices.

--- src/js/app.js
+++ src/js/app.js
@@ -2,17 +2,19 @@ import Rx from "rxjs";
 
 const btn = document.querySelector("#btn");
 
+const eventObserveble = Rx.Observable.fromEvent(btn, "click")
+    .scan(val => val + 1, 0)
+    .map(val => val ** 2);
+
+eventObserveble.subscribe(val => console.log("val", val));
+
 const observeble = Rx.Observable.create(observer => {
-    observer.next(1);
+    observer.next(2);
     setTimeout(() => {
         observer.next(4);
-    }, 1000);
-});
-
-btn.onclick = () => {
-    let count = 0;
-    observeble.next(++count);
-};
+    }, 5000);
+    observer.next(18);
+}).map(val => val ** 2);
 
 observeble.subscribe(x => {
     console.log("got value", x);
