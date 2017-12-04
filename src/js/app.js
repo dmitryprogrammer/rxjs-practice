ef74d4f63bb294b17bc1574fd30191595061de5d
dpischalka <pischalkads@altarix.ru>
2017-12-04 19:05:31 +0400

    writing two flows from memory and trying to understand how method add works.

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+import Rx from "rxjs";
+
+const observer = Rx.Observable.of([1, 2, 3]);
+const observer2 = Rx.Observable.create((observer) => {
+    observer.next(1);
+    observer.next(2);
+    observer.next(3);
+    setTimeout(function () {
+        observer.next(6);
+    }, 2100);
+    setTimeout(function () {
+        observer.next(5);
+    }, 500);
+    observer.next(4);
+});
+
+const subscription = observer.subscribe(x => console.log(x));
+const subscription2 = observer2.subscribe(y => console.log(y));
+
+setTimeout(() => {
+    subscription.unsubscribe();
+    subscription2.unsubscribe();
+}, 2000);
