742333bc6ab172759085cc3ecf24b3dc2417a0a8
dpischalka <pischalkads@altarix.ru>
2017-12-05 10:44:47 +0400

    practicing with subjects in RxJS.

--- src/js/subject.js
+++ src/js/subject.js
@@ -1,5 +1,28 @@
+import Rx from "rxjs";
+
 export class SubjectsPractice {
     constructor() {
-        console.log("hello subjects");
+        const subject = new Rx.Subject();
+
+        //  subscribing
+        subject.subscribe({
+            next: (v) => console.log("observable 1", v)
+        });
+
+        setTimeout(() => {
+            subject.subscribe((v) => console.log("observable 2", v));
+        }, 2000);
+
+        subject.next(1);
+        subject.next("Family");
+        subject.next(2);
+        setInterval(function () {
+            subject.next({
+                a: 1,
+                b: 2,
+                c: 3,
+                d: 4
+            });
+        }, 5000);
     }
 }
