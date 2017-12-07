22343b632d7dca5b08611757aecfb2d17d4d53d9
dpischalka <pischalkads@altarix.ru>
2017-12-07 17:22:06 +0400

    with example of the docs about Subjects.

--- src/js/subject.js
+++ src/js/subject.js
@@ -31,5 +31,19 @@ export class SubjectsPractice {
                 payload: "test mouse down"
             });
         });
+
+
+        const source = Rx.Observable.from([1, 2, 3]);
+        const subject2 = new Rx.Subject();
+        const multicasted = source.multicast(subject2);
+
+        // These are, under the hood, `subject.subscribe({...})`:
+        multicasted.subscribe({
+            next: (v) => console.log('observerA: ' + v)
+        });
+        multicasted.subscribe({
+            next: (v) => console.log('observerB: ' + v)
+        });
+
     }
 }
